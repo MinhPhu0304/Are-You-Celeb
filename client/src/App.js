5651e6212dca751569da3803b943056485b874cd
Minh-Phu <Lminhphu0304@gmail.com>
2019-06-28 12:04:19 +1200

    Add some simple styling
    It's still ugly but it has loading effect which is nice

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Dropzone from 'react-dropzone'
 
 import './App.css';
+import axios from 'axios'
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      startAnalyzingPic: false
+    }
+  }
 
   handleFileUpload = file => {
     // Only 1 file is allowed so the array only has 1 element
@@ -13,24 +26,43 @@ class App extends React.Component {
     fileReader.readAsBinaryString(image)
     fileReader.addEventListener('load', ({currentTarget}) => { // Nasty destructuring but it looks nice
       const { result } = currentTarget
+      this.setState({startAnalyzingPic : !this.state.startAnalyzingPic})
+      setTimeout(() => this.setState({startAnalyzingPic: !this.state.startAnalyzingPic}),1000)
       // TODO: Throw that to the back end using axios or what ever
     })
   }
 
+
+
   render(){
     return (
       <div className="App">
-        
-        <Dropzone  multiple={false} onDrop={this.handleFileUpload}>
-          { ({getRootProps, getInputProps}) => (
-            <section >
-              <div className="Dropzone" {...getRootProps()}>
-                <input {...getInputProps()} />
-                <p id="uploadDesciption">Drag'n drop some image to see the magic here</p>
-              </div>
-            </section>
-          )}
-        </Dropzone>
+        <Grid container alignContent="center" direction="column">
+          <Paper style={{}}>
+            <Typography variant="h5" component="h3" color="primary">
+              Let's see if the image you upload is from a well known celebrity
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid container direction="column" justify="space-between" alignItems="stretch">
+          <Box>
+            <Grid>
+              <Dropzone  multiple={false} onDrop={this.handleFileUpload} style={{margin: 4}}>
+                { ({getRootProps, getInputProps}) => (
+                  <Box boxShadow={3}>
+                    <div className="Dropzone" {...getRootProps()}>
+                      <input {...getInputProps()} />
+                      <p id="uploadDesciption">Drag'n drop some image to see the magic here</p>
+                    </div>
+                  </Box>
+                )}
+              </Dropzone>
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid>
+          { this.state.startAnalyzingPic ? (<CircularProgress style={{margin: '10px'}}/>) : ''}
+        </Grid>
       </div>
     );
   }
